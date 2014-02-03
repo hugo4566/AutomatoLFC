d1c9f60b3a184f3b64513cb2670f7bf366fe1f8f
Hugo Prado Figueiredo <hugo4566@gmail.com>
2014-02-03 20:03:14 -0300

    Criada uma lista e passada as informacoes do parser para a lista

--- AutomatoPilha/src/projeto/Principal.java
+++ AutomatoPilha/src/projeto/Principal.java
@@ -2,18 +2,24 @@ package projeto;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Principal {
 
 	public static void main (String [] args) throws FileNotFoundException {
 		
+		ArrayList<String> Lista = new ArrayList<String>();
+		
 		// Parser
 		Scanner scanner = new Scanner(new FileReader("arquivo.txt"));
 		scanner.useDelimiter(",");
 		while (scanner.hasNext()) {
-			String nome = scanner.next();
-			System.out.println(nome);
+			Lista.add(scanner.next());
+		}
+		
+		for(String lista :Lista){
+			System.out.println(lista);
 		}
 		
 		
